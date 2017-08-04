63a68086e345bc4806e9255a1cf868065bd72d9f
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2017-08-04 15:34:14 +0200

    testcase logic to mock out the UBlox device / class

---
+++ test_ublox.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import patch, Mock
+"""
+Test code to mock out the need for the UBlox class to actually be connected ¨
+the the EVK 8N device.
+"""
+
+
+class UBloxTest(TestCase):
+    
+    @patch('ublox.UBlox')
+    def test_ublox(self, UBloxMock):
+        dev = UBloxMock()
+        dev.name.return_value = "MockDevice"
+
+        self.assertIsNotNone(dev.name())
