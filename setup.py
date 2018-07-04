89b69f9094c7b7fa1b84c5b9e8b164ba73fe6732
william <william.flynt@gmail.com>
2018-07-04 08:54:03 -0700

    updated keywords

--- setup.py
+++ setup.py
@@ -9,9 +9,8 @@ setup(
     description="python uBlox module",
     author_email="william@williamflynt.com",
     url="https://github.com/williamflynt/pyUblox",
-    keywords=["uBlox", "ubx"],
-    packages=find_packages(),
-    package_data={'': ['swagger/swagger.yaml']},
+    keywords=["uBlox", "ublox", "ubx", "py-ublox", "pyublox"],
+    packages=['ublox'],
     include_package_data=True,
     long_description=""" """
 )
