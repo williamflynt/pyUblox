548af348f199226ba7d0f7f8ce62fda39be65985
william <william.flynt@gmail.com>
2018-07-04 09:51:04 -0700

    Added NTRIPError exception class

--- ublox/ntrip.py
+++ ublox/ntrip.py
@@ -4,20 +4,27 @@ import socket
 import base64
 import sys
 
+
+# Added because I couldn't find this class anywhere, but this script
+# raises it on some errors.
+class NTRIPError(Exception):
+    pass
+
+
 dummy, server, port, username, password, mountpoint = sys.argv
 
 pwd = base64.b64encode("{}:{}".format(username, password))
 
-header =\
-"GET /{} HTTP/1.1\r\n".format(mountpoint) +\
-"Host \r\n".format(server) +\
-"Ntrip-Version: Ntrip/2.0\r\n" +\
-"User-Agent: NTRIP pyUblox/0.0\r\n" +\
-"Connection: close\r\n" +\
-"Authorization: Basic {}\r\n\r\n".format(pwd)
+header = \
+    "GET /{} HTTP/1.1\r\n".format(mountpoint) + \
+    "Host \r\n".format(server) + \
+    "Ntrip-Version: Ntrip/2.0\r\n" + \
+    "User-Agent: NTRIP pyUblox/0.0\r\n" + \
+    "Connection: close\r\n" + \
+    "Authorization: Basic {}\r\n\r\n".format(pwd)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server,int(port)))
+s.connect((server, int(port)))
 s.send(header)
 
 resp = s.recv(1024)
@@ -33,19 +40,17 @@ try:
         # seems more robust to simply let the higher level RTCMv3 parser
         # frame everything itself and bin the garbage as required.
 
-        #length = s.recv(4)
+        # length = s.recv(4)
 
-        #try:
+        # try:
         #    length = int(length.strip(), 16)
-        #except ValueError:
+        # except ValueError:
         #    continue
 
         data = s.recv(1024)
         print(data)
-        #print >>sys.stderr, [ord(d) for d in data]
+        # print >>sys.stderr, [ord(d) for d in data]
         sys.stdout.flush()
 
 finally:
     s.close()
-
-
