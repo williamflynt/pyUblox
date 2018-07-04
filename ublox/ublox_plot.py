3353ccb5eca1c5d3531bf446333edc6a750a4bcd
william <william.flynt@gmail.com>
2018-07-04 09:16:30 -0700

    relative imports, py3 compatibility with raw_input --> input

--- ublox/ublox_plot.py
+++ ublox/ublox_plot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import ublox, sys, fnmatch, os, time
-import numpy, util, math
-import matplotlib
-from matplotlib import pyplot
-from matplotlib.lines import Line2D
-
+import math
 from optparse import OptionParser
+
+from matplotlib import pyplot
+
+import time
+from . import ublox, util
 
 parser = OptionParser("ublox_plot.py [options] <file>")
 parser.add_option("--seek", type='float', default=0, help="seek percentage to start in log")
@@ -25,7 +25,7 @@ else:
 # create a figure
 f = pyplot.figure(1)
 f.clf()
-pyplot.axis([-opts.size,opts.size,-opts.size,opts.size])
+pyplot.axis([-opts.size, opts.size, -opts.size, opts.size])
 pyplot.ion()
 
 colours = ['ro', 'bo', 'go', 'yo']
@@ -40,29 +40,32 @@ if opts.seek != 0:
 
 last_t = time.time()
 
+
 def get_xy(pos, home):
     distance = util.gps_distance(home[0], home[1], pos[0], pos[1])
     bearing = util.gps_bearing(home[0], home[1], pos[0], pos[1])
     x = distance * math.sin(math.radians(bearing))
     y = distance * math.cos(math.radians(bearing))
-    return (x,y)
+    return (x, y)
+
 
 def plot_line(pos1, pos2, home, colour):
     global last_t
-    (x1,y1) = get_xy(pos1, home)
-    (x2,y2) = get_xy(pos2, home)
-    pyplot.plot([x1,x2], [y1,y2], colour, linestyle='solid', marker=None, alpha=0.1)
+    (x1, y1) = get_xy(pos1, home)
+    (x2, y2) = get_xy(pos2, home)
+    pyplot.plot([x1, x2], [y1, y2], colour, linestyle='solid', marker=None, alpha=0.1)
     t = time.time()
     if t - last_t > 1:
         pyplot.draw()
         pyplot.show()
         last_t = t
 
-poscount = [0]*len(devs)
+
+poscount = [0] * len(devs)
 home = None
 if reference_position:
     home = (reference_position.lat, reference_position.lon)
-last_pos = [None]*len(devs)
+last_pos = [None] * len(devs)
 
 while True:
     got_eof = 0
@@ -77,7 +80,7 @@ while True:
             break
         if msg.name() == 'NAV_POSLLH':
             msg.unpack()
-            pos = (msg.Latitude*1.0e-7, msg.Longitude*1.0e-7)
+            pos = (msg.Latitude * 1.0e-7, msg.Longitude * 1.0e-7)
             if home is None:
                 home = pos
             if last_pos[i] is None:
@@ -89,5 +92,4 @@ while True:
     if got_eof == len(devs):
         break
 f.show()
-raw_input('Press enter')
-
+input('Press enter')
